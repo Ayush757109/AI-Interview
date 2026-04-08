09346601d489eff2ddf5faddc1f0a1d8f7b4140e
Ayush Maurya <ayushmaurya2112@gmail.com>
2026-04-08 20:00:32 +0530

    feat: integrate Supabase database using Prisma ORM

--- prept/components/Header.jsx
+++ prept/components/Header.jsx
@@ -3,15 +3,25 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   const { isSignedIn } = useUser();
 
   return (
     <nav className="fixed top-0 inset-x-0 z-50 flex items-center justify-between px-3 sm:px-10 py-3 border-b border-white/10 backdrop-blur-xl">
-      
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="Prept Logo"
+          width={100}
+          height={100}
+          className="h-11 w-auto"
+        />
+      </Link>
       <div className="text-lg font-semibold">
-        MyApp
+        Prept
       </div>
 
       <div className="flex items-center gap-3">
